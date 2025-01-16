f869e8ddf9e683b96603df51cf305e57a723f5e1
李斯图 <3059808984@qq.com>
2025-01-16 17:21:25 +0800

    Add files via upload

--- 1.16.py
+++ 1.16.py
@@ -1,4 +1,16 @@
+'''
 money = 50
 print("钱包还有：",money)
 money = money-10
 print("买完冰淇淋后，还剩",money,"元")
+'''
+
+m = 50
+print("当前金额剩余：",m,"元")
+b = 10
+print("冰淇淋花费：",b,"元")
+c = 5
+print("可乐花费：",c,"元")
+M = m-b-c
+print("最后钱包还剩：",M,"元")
+
